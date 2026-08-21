27f90a75cd7233ee8cc9a6f38e843af9281eb8ed
Sireesha1823 <venganapallisireesha18@gmail.com>
2026-08-21 11:14:10 +0530

    Added Ex 11_Top-Down Parser for Context-Free Grammar

---
+++ Lab Programs/download_nltk.py
@@ -0,0 +1,8 @@
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('maxent_ne_chunker_tab')
+nltk.download('words')
